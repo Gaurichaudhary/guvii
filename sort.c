ae8aad898eaa50b3461243b5046f85c8e5f53048
Gaurichaudhary <35339147+Gaurichaudhary@users.noreply.github.com>
2018-01-14 01:49:28 -0800

    Create sort.c

---
+++ sort.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{  int a[100000],n,i,j,temp;
+   printf("Enter the value of n\n");
+   scanf("%d",&n);
+   if(n>=1&&n<=100000){
+   printf("Enter the elements\n");
+   for(i=0;i<n;i++)
+   scanf("%d ",&a[i]);
+   for(i=0;i<n;i++)
+   {for(j=0;j<n-i-1;j++)
+    {  if(a[j]>a[j+1])
+         {  temp=a[j];
+            a[j]=a[j+1];
+            a[j+1]=temp;} }
+       
+   }
+   for(i=0;i<n;i++)
+   printf("%d ",a[i]);
+}}
