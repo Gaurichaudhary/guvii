17415f540a5ab87a9e43bc9728dfdb51d9f0db7c
Gaurichaudhary <35339147+Gaurichaudhary@users.noreply.github.com>
2018-01-14 01:38:10 -0800

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{  int a[100000],n,i,max;
+   printf("Enter the value of n\n");
+   scanf("%d",&n);
+   if(n>=1&&n<=100000){
+   printf("Enter the elements\n");
+   for(i=0;i<n;i++)
+   scanf("%d ",&a[i]);
+   for(i=0;i<n;i++)
+   {
+        if(max<a[i])
+        max=a[i];
+   }
+   printf("%d",max);}
+}
