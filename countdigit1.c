762273d910cd9f9aa1317f81face0aaf69c025c6
Gauri Chaudhary <35339147+Gaurichaudhary@users.noreply.github.com>
2018-01-21 01:34:54 -0800

    Update countdigit1.c

--- countdigit1.c
+++ countdigit1.c
@@ -6,10 +6,11 @@ int main()
     char str[1000];
     int i,count=0;
     gets(str);
+   if(strlen(str)<=1000){
     for(i=0;i<strlen(str);i++)
     {  if(isdigit(str[i]))
          count++; }
     printf("%d",count);
     
-    return 0;
+    return 0;}
 }
