85abffa87366aec6bcb95980e2ae7bd7a2257289
Gauri Chaudhary <35339147+Gaurichaudhary@users.noreply.github.com>
2018-01-21 07:51:58 -0800

    Create camelcase.c

---
+++ camelcase.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char  a[1000000];
+    int i;
+    scanf("%[^\n]s",&a);
+    if(strlen(a)<=1000000){
+        a[0]=a[0]-32;
+    for(i=0;i<strlen(a);i++)
+    {  if(a[i]==' '||a[i]=='\t'||a[i]=='\n')
+        a[i+1]=a[i+1]-32;
+    }
+    printf("%s",a);
+    return 0;}
+}
