4758b353c4b5fec458767499e4995b9a48588557
Gauri Chaudhary <35339147+Gaurichaudhary@users.noreply.github.com>
2018-01-22 07:56:43 -0800

    Update countword.c

--- countword.c
+++ countword.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+#include<string.h>
 
 int main()
 {    char a[100];
      int i,count=0;
      scanf("%[^\t]s",a);
+     if(strlen(a)<=1000){
      for(i=0;a[i]!=NULL;i++){
          if(a[i]==' ')
          count++;}
          printf("%d",(count+1));
      
 
-    return 0;
+    return 0;}
 }
